169adf4da738690be5ec0d9855254e45baca3911
Youness Lamraoui <ylamraou@e3r4p14.1337.ma>
2023-12-17 00:54:35 +0100

    +42

--- srcs/requirements/mariadb/tools/script_mariadb.sh
+++ srcs/requirements/mariadb/tools/script_mariadb.sh
@@ -1,28 +1,17 @@
 #!/bin/bash
 
-# Update MariaDB configuration
 sed -i "s/bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mariadb.conf.d/50-server.cnf
 
-# Create and set permissions for MySQL data directory
 mkdir -p /var/lib/mysql
 chown -R mysql:mysql /var/lib/mysql
 
-# Create and set permissions for MySQL run directory
 mkdir -p /var/run/mysqld
 chown -R mysql:mysql /var/run/mysqld
 
-# Start MariaDB service
 service mariadb start
 
-# Sleep for a moment to ensure MariaDB has started
 sleep 5
 
-# Run SQL commands to set up databases and users
-# echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;
-# CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
-# GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%';
-# ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_PASSWORD_ROOT';
-# FLUSH PRIVILEGES;" | mysql -u root -p$DB_PASSWORD_ROOT
 
 echo "CREATE DATABASE IF NOT EXISTS $DB_NAME; \
 CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD'; \
@@ -31,14 +20,8 @@ GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%'; \
 GRANT SELECT ON $DB_NAME.* TO '$DB_USER_2'@'%'; \
 FLUSH PRIVILEGES;" |  mysql -u root -p$DB_PASSWORD_ROOT
 
-# Stop MariaDB service
 service mariadb stop
 
-# Sleep again before potentially terminating the script
 sleep 3
 
-# Uncomment the following line if you want to kill the mysqld process
-# kill $(cat /var/run/mysqld/mysqld.pid)
-
-# Start mysqld in the foreground (not recommended for production use)
 mysqld
